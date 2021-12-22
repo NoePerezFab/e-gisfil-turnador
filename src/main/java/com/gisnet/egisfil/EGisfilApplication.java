8b8c6d00ad9a9afd986a6b14a9c57d45f83db266
NoePerezFab <noeperez@e-gisnet.com>
2021-12-21 19:25:49 -0600

    Version de produccion

--- src/main/java/com/gisnet/egisfil/EGisfilApplication.java
+++ src/main/java/com/gisnet/egisfil/EGisfilApplication.java
@@ -3,15 +3,21 @@ package com.gisnet.egisfil;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 
 @ComponentScan(basePackages = {"com.gisnet.egisfil"})
-public class EGisfilApplication {
+public class EGisfilApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(EGisfilApplication.class, args);
 	}
+         @Override
+        protected SpringApplicationBuilder configure(SpringApplicationBuilder builder){
+            return builder.sources(EGisfilApplication.class);
+        }
 
 }
